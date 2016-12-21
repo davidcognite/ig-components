f19284038b3ba41b3772af47d1526383340b9d34
David Webster <david@cognite.net>
2016-12-21 21:23:34 +0000

    Duplicate CSS and template code in lieu of inheritance and bump version number

--- rm-file-plan/src/rm-file-plan.component.ts
+++ rm-file-plan/src/rm-file-plan.component.ts
@@ -4,6 +4,8 @@ import {DocumentList} from 'ng2-alfresco-documentlist'
 
 @Component({
     selector: 'rm-file-plan',
+    templateUrl: './rm-file-plan.component.html',
+    styleUrls: ['./rm-file-plan.component.css']
 })
 export class RmFilePlanComponent extends DocumentList {
 
