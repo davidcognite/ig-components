b228650efd974bf215985d66dfc7dd24041ce529
David Webster <david@cognite.net>
2016-12-21 16:08:21 +0000

    rm-file-plan extends ng2-alfresco-documentlist

--- rm-file-plan/src/rm-file-plan.component.ts
+++ rm-file-plan/src/rm-file-plan.component.ts
@@ -1,9 +1,10 @@
 
 import { Component } from '@angular/core';
+import {DocumentList} from 'ng2-alfresco-documentlist'
 
 @Component({
     selector: 'rm-file-plan',
-    styles: [`:host h1 { font-size:22px }`],
-    template: `<h1>Hello World Angular 2 rm-file-plan</h1>`
 })
-export class RmFilePlanComponent {}
+export class RmFilePlanComponent extends DocumentList {
+
+}
